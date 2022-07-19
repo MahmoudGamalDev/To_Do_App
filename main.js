6ba3dda33c72be5836dbaf52231810d9ed12c1ff
Jimmy <mahmoudgamalawad10@gmail.com>
2022-07-19 06:46:43 +0200

    Keeping tasks in page on reload

--- main.js
+++ main.js
@@ -1,14 +1,11 @@
 const task = document.querySelector(".input");
 const add = document.querySelector(".add");
 const tasksHolder = document.querySelector(".tasks-holder");
+let tasksArray = [];
 
-add.onclick = () => {
-  if (task.value !== "") {
-    createTask(task.value);
-  }
-};
 
 let createTask = (taskText) => {
+  // Adding the task to page
   let MainDiv = document.createElement("div");
   MainDiv.className = "task";
 
@@ -31,6 +28,39 @@ let createTask = (taskText) => {
   MainDiv.appendChild(buttons);
 
   tasksHolder.appendChild(MainDiv);
+};
 
-  task.value = ""
+
+if (window.localStorage.getItem("tasks")) {
+  tasksArray = JSON.parse(window.localStorage.getItem("tasks"));
+
+  for (let i = 0; i < tasksArray.length; i++) {
+    createTask(tasksArray[i].taskName)
+  }
+}
+
+add.onclick = () => {
+  if (task.value !== "") {
+    addTaskToArray(task.value);
+    task.value = "";
+  }
+};
+
+let addTaskToArray = (taskText) => {
+  let newTask = {
+    id: Date.now(),
+    taskName: taskText,
+    completed: false,
+  };
+  tasksArray.push(newTask);
+
+  createTask(newTask.taskName);
+  addTasksArrayToLocalStorage("tasks", JSON.stringify(tasksArray));
+};
+
+
+console.log(tasksArray);
+
+let addTasksArrayToLocalStorage = (key, value) => {
+  window.localStorage.setItem(key, value);
 };
